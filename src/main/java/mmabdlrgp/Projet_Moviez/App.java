034f82f670026426ce7084d379d345c9af34bf40
agimenez <agimenez@pomerol.educ.insa>
2017-11-29 08:41:04 +0100

    Initial commit

---
+++ src/main/java/mmabdlrgp/Projet_Moviez/App.java
@@ -0,0 +1,15 @@
+package mmabdlrgp.Projet_Moviez;
+
+import org.apache.spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
