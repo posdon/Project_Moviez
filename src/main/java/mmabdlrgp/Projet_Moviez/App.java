bcd88fe80916ce2750991f00dd502dc293a634ef
agimenez <agimenez@vittel.educ.insa>
2017-11-29 14:53:22 +0100

    Creation de SparkSession

--- src/main/java/mmabdlrgp/Projet_Moviez/App.java
+++ src/main/java/mmabdlrgp/Projet_Moviez/App.java
@@ -1,6 +1,11 @@
 package mmabdlrgp.Projet_Moviez;
 
-import org.apache.spark.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.SparkSession;
 
 /**
  * Hello world!
@@ -10,6 +15,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	// Creation de session spark.
+    	// Example : https://github.com/apache/spark/blob/master/examples/src/main/java/org/apache/spark/examples/sql/JavaSparkSQLExample.java
+    	SparkSession spark = SparkSession
+    		      .builder()
+    		      .appName("Java Spark SQL basic example")
+    		      .config("spark.some.config.option", "some-value")
+    		      .master("local")
+    		      .getOrCreate();
+        
+		SQLContext sqlContext = new SQLContext(spark);
+		String path = "./movies.csv";
+		Dataset<Row> products = sqlContext.load(path,"csv");
+		System.out.println(products.first());
+
     }
 }
